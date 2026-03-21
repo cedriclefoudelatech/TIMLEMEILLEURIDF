dbbac4435f71b02fba2840cedcf223459f855535
cedriclefoudelatech <Doumbia9320@gmail.com>
2026-03-21 19:29:48 +0100

    Cleanup: Remove temporary scripts

--- tmp/check_db_tim.js
+++
@@ -1,8 +0,0 @@
-const { getAppSettings } = require('../services/database');
-
-async function check() {
-    console.log('--- DB Check for TIM ---');
-    const settings = await getAppSettings();
-    console.log('Admin IDs:', settings.admin_telegram_id);
-}
-check();
